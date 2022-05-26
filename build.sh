3b0be936ea4ca5d0b725120a65b96d1ceb59a5a8
DESKTOP-HCVBQJH\danil <danil_grezev@mail.ru>
2022-05-26 12:14:46 +0700

    Temp

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export buildDate=$(date '+%Y-%m-%d_%H:%M:%S') 
-export gitcommithash=$(git rev-parse HEAD)
+export gitcommithash=$(git rev-parse HEAD) 
 docker compose build
 docker compose up
